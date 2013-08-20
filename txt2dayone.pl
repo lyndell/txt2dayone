40b29bde08170498e86f8f81606c06a36e5daa2b
Lyndell Rottmann <lrottmann@softlayer.com>
2013-08-20 18:05:17 -0500

    revisions, works

--- txt2dayone.pl
+++ txt2dayone.pl
@@ -5,10 +5,58 @@ use warnings;
 use strict;
 
 my $date = "Jan 1999";
-my $file = "null";
+my $file = "empty.doh";
+
+my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+         $atime,$mtime,$ctime,$blksize,$blocks);
+
+if ( -e $file ) {
+
+  ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+         $atime,$mtime,$ctime,$blksize,$blocks)
+             = stat($file);
+
+  $date = $ctime;
+
+  print "dayone -d=$date new < $file \n";
+  exit;
+
+  system(" dayone -d=\"$date\" new < $file ") or die $! ;
+} else {
+  print "$file missing.  $!  \n\n"; 
+}
 
 
-$dayoneman = <<DAYONE;
+
+exit;
+
+
+
+my $stathelp=<<'STATHELP';
+
+  0 dev      device number of filesystem
+  1 ino      inode number
+  2 mode     file mode  (type and permissions)
+  3 nlink    number of (hard) links to the file
+  4 uid      numeric user ID of file's owner
+  5 gid      numeric group ID of file's owner
+  6 rdev     the device identifier (special files only)
+  7 size     total size of file, in bytes
+  8 atime    last access time in seconds since the epoch
+  9 mtime    last modify time in seconds since the epoch
+ 10 ctime    inode change time in seconds since the epoch (*)
+ 11 blksize  preferred I/O size in bytes for interacting with the
+             file (may vary from file to file)
+ 12 blocks   actual number of system-specific blocks allocated
+             on disk (often, but not always, 512 bytes each)
+
+STATHELP
+
+
+
+
+
+my $dayone=<<'DAYONE';
 
 # Commands
 
@@ -17,7 +65,7 @@ new
   Adds a new entry
   using text from stdin
 
-$ Options
+# Options
 
 -d,
 –date=<date>
@@ -31,9 +79,4 @@ $ Options
   -[NSDate dateWithNaturalLanguageString].
 
 
-DAYONE:
-
-print " dayone -d=\"$date\" new < $file \n";
-exit;
-
-system(" dayone -d=\"$date\" new < $file ") or die $! ;
+DAYONE
