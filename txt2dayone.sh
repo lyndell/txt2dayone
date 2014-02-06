5fa17656ea4768936365b6be8a25d3caa4db7395
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2014-02-05 23:17:56 -0600

    syntactically correct in .sh

--- txt2dayone.sh
+++ txt2dayone.sh
@@ -4,29 +4,40 @@
 #
 
 date=""
-file=""
+file=$1
 
 # Check for journal entry
 #
-if [ -f $file ] then
-  echo $file exists!
+if [ -f $file ]
+then
+  echo " $file exists! "
 fi
+
+# Get date of file
+#
+#
+# tech@wrkstn txt2dayone $ stat -c %y empty.txt 
+# 2013-12-25 17:20:10.000000000 -0600
+# tech@wrkstn txt2dayone $ 
+
+date=`stat -c %y $file `
+
 
 # Check for corresponding phots, with same file name as
 # entry
 #
+
+photoCmd=""
+
 # # Test mode
 #
 # Print commands that will be run
-#
-#
-#
 # # Run mode
 #
 # Add jounral entry
 #
 
-dayone $photoCmd -d=\"${date}\" new < \"${file}\" 
+dayone  -d=\"${date}\" new < ${file} 
 
 # Delete added files, including photo if present.
 #
