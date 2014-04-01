f61e343d33bdd205fd5f4de47c63b42df67160e4
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2014-04-01 11:55:06 -0500

    break into seperate functions

--- txt2dayone.sh
+++ txt2dayone.sh
@@ -7,6 +7,20 @@ date=""
 file=$1
 
 # FUNCTIONS
+
+
+function getFile () {
+  if [ -z $file ]
+  then
+    echo "No file specified."
+  fi
+
+  if [ ! -e $file ] # no file
+  then
+    echo "File missing or not found."
+  fi
+
+}
  
 # make sure stat command is installed 
 function checkstat () {
@@ -80,7 +94,10 @@ function checkTrash () {
 
 checkstat
 checkDayone
+
+getFile
 getDate
+getPhoto 
 
 # Check for journal entry
 #
@@ -96,3 +113,39 @@ fi
 #
 # Add jounral entry
 #
+
+
+
+
+
+DAYONE=<<'DAYONE';
+
+# Commands
+
+new
+
+  Adds a new entry
+  using text from stdin
+
+# Options
+
+-d,
+–date=<date>
+
+  Creation date of the entry. If
+  not specified then the current date is used. This option
+  accepts dates in most formats, including some natural
+  language strings such as ’tomorrow’ and
+  ’yesterday’. The systemâs locale is used
+  when deciding how to parse a date. The date is parsed using
+  -[NSDate dateWithNaturalLanguageString].
+
+-p,
+–photo-path=<path>
+
+  File path to a photo to attach
+  to entry. Most image formats are accepted. If any side of
+  the image is greater than 1600 pixels then it will be
+  resized. In all cases the image is converted to JPEG.
+
+DAYONE
